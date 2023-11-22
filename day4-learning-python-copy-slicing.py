80992da04d24774d3fb1a756e0abeefced925e33
anoyfloyd <143277482+anoyfloyd@users.noreply.github.com>
2023-11-23 04:24:58 +0530

    Add files via upload

---
+++ day4-learning-python-copy-slicing.py
@@ -0,0 +1,65 @@
+print('my name is Ananyo .')
+a = 10
+print(a)
+annie = 11
+print(annie)
+b = 23 
+print(b)
+s = 'pwskills'
+type(s)
+# indexing . forward indexing .
+s[0]
+s[1]
+s[2]
+s[3]
+s[4]
+# indexing . backward indeing .
+s[-1]
+s[-6]
+s[-8]
+s[0:3]
+# slicing  = (upper bound - 1 )
+s[0:4]
+s[0:8:2]
+s[::2]
+s[2::2]
+#slice stape cant be 0
+
+# reverse direction print 
+s[::-1]
+s[2:7:-1]
+# starting from 2nd index going till 7th index but jumping in negetive direction 
+# so basically it will give you blank 
+s[8:0:-1]
+s[-2:-8:-1]
+s[::-1]
+s[::1]
+s[:-90:]
+s[:-90:-1]
+# in case of slicing upper bound doesnt matter . but in case of indexing it matters 
+len(s)
+s.find('s')
+s.find('i')
+s1 = 'I love momo .'
+s1.find('l')
+s1.count('s')
+s1.count('m')
+s1.upper()
+s1.lower()
+s1.title()
+s1.capitalize()
+s + '1'
+# concatination 
+s = s + ' ineuron'
+s
+# you can only concatinate str with str 
+s * 3
+l = " don't do copy and paste in my class . "
+l.count('o')
+l.count('do')
+l.count('copy')
+''' this is my first class of programming 
+ive declared a data type 
+learnt about slicing 
+'''
+
